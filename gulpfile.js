e727cffc8450fef058926594352e65402d253da0
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-10-12 14:47:17 +0400

    #39107 add es-seat,  and update gulpfile, prepare to add directive

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-(function() {
+(function () {
 
     var gulp = require('gulp'),
         del = require('del'),
+        browserify = require('browserify'),
+        strictify = require('strictify'),
+        buffer = require('vinyl-buffer'),
+        source = require('vinyl-source-stream'),
+        sourcemaps = require('gulp-sourcemaps'),
         ngHtml2Js = require('gulp-ng-html2js'),
         minifyHtml = require('gulp-minify-html'),
         uglify = require('gulp-uglify'),
@@ -9,11 +14,11 @@
         stylus = require('gulp-stylus'),
         autoprefixer = require('autoprefixer-stylus');
 
-    gulp.task('clean', function() {
+    gulp.task('clean', function () {
         return del('build');
     });
 
-    gulp.task('build:html', function() {
+    gulp.task('build:html', function () {
         return gulp.src('src/**/*.html')
             .pipe(minifyHtml({
                 empty: true,
@@ -22,7 +27,7 @@
             }))
             .pipe(ngHtml2Js({
                 moduleName: 'app',
-                rename: function(url) {
+                rename: function (url) {
                     return url.replace('src/', '');
                 }
             }))
@@ -32,11 +37,23 @@
     });
 
 
-    gulp.task('watch', function() {
-        gulp.watch('src/**/*.*', gulp.series('build:html'));
+    gulp.task('build:js', function () {
+        return browserify('src/index.js', {transform: strictify})
+            .bundle()
+            .pipe(source('controllers-redwings.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('build'))
     });
 
-    gulp.task('build', gulp.series('build:html'));
+
+    gulp.task('watch', function () {
+        gulp.watch('src/**/*.*', gulp.series('build:html', 'build:js'));
+    });
+
+    gulp.task('build', gulp.series('build:html', 'build:js'));
 
     gulp.task('default', gulp.series('build', 'watch'));
 
